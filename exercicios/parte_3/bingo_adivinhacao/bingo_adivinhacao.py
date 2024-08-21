238bd68dd82e478637b04afe5d01bd6e0d705060
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-08-21 12:21:21 -0300

    Finalizando o exercicio do bingo

---
+++ exercicios/parte_3/bingo_adivinhacao/bingo_adivinhacao.py
@@ -0,0 +1,28 @@
+import random
+
+cartela = list()
+sorteios = 0
+
+for aleatorio in range(1,6):
+    cartela.append(random.randint(1,75))
+
+print(f'Sua cartela é : {cartela}') 
+
+while True:
+    numero = random.randint(1,75)
+
+    if numero in cartela:
+        print(f'O número sorteado foi: {numero} e você acertou !!!')
+        while numero in cartela:
+            cartela.remove(numero)
+        print(f'Sua cartela está assim: {cartela}')
+    else:
+        print(f'O número sorteado foi: {numero}')
+
+    sorteios +=1
+
+    if not cartela:
+        print(f'Parabéns ! , você completou em {sorteios} sorteios.')
+        break
+
+
